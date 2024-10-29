edbb2e69274741a5bffc9cf240dae7de2db365ac
hamil2nc <hamil2nc@dukes.jmu.edu>
2024-10-29 11:26:54 -0400

    10/29

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -1,4 +1,3 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,13 +5,18 @@ import numpy as np
 path = '../../../data/ekg/mitdb_201.csv'
 
 # load data in matrix from CSV file; skip first two rows
-
 ### Your code here ###
+data = np.loadtxt(path, delimiter=',', skiprows=2)
 
 # save each vector as own variable
-
 ### Your code here ###
+sample = 3000
+
+time = data[:sample, 0]
+voltage = data[:sample, 2]
 
 # use matplot lib to generate a single
-
 ### Your code here ###
+plt.plot(time, voltage, label='EKG Data')
+plt.legend()
+plt.show()
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -18,8 +18,8 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
-## YOUR CODE HERE ##
+signal = np.loadtxt(signal_filepath, delimiter=',', skiprows=2)
+
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
@@ -32,18 +32,23 @@ Step 3: Pass data through weighted differentiator
 """
 
 ## YOUR CODE HERE ##
-
+np.diff(signal)
 
 """
 Step 4: Square the results of the previous step
 """
  ## YOUR CODE HERE ##
+np.square(signal)
 
 """
 Step 5: Pass a moving filter over your data
 """
 
 ## YOUR CODE HERE
+window_size = 10
+window_array = np.ones(window_size, dtype=int)
+signal = np.convolve(signal, window_array)
+
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
 plt.title('Process Signal for ' + database_name)
 plt.plot(signal)
